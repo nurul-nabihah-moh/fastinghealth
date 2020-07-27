58b724b9be9c23e9eebe49e954d6107c0bac049d
nurul-nabihah-moh <amrinabihah@gmail.com>
2020-07-27 11:35:55 +0800

    Initial commit

---
+++ lib/splashscreen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:fastinghealth/loginscreen.dart';
+
+
+void main() => runApp(SplashScreen());
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double screenHeight;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 100),
+                Image.asset(
+                  'assets/images/splashscreen.jpeg',
+                  width: 300,
+                  height: 300,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(height: 200, child: ProgressIndicator())
+                  ],
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class ProgressIndicator extends StatefulWidget {
+  @override
+  _ProgressIndicatorState createState() => new _ProgressIndicatorState();
+}
+
+class _ProgressIndicatorState extends State<ProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = Tween(begin: 0.0, end: 1.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          if (animation.value > 0.99) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => LoginScreen()));
+          }
+        });
+      });
+    controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    controller.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Container(
+      child: CircularProgressIndicator(
+        value: animation.value,
+        backgroundColor: Colors.lightBlueAccent,
+        valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+      ),
+    ));
+  }
+}
